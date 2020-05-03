2322cc324f254418f8ef05885533df8ae08bdd79
Joshua Singleton <jsingle345@gmail.com>
2020-05-03 03:27:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 
 def reverse_each_word(string)
-  new_array = []
+  new_array = " "
   array = string.split(" ")
   array.collect { |string| string.reverse } 
   new_array << [array.collect { |string| string.reverse}].join(" ")

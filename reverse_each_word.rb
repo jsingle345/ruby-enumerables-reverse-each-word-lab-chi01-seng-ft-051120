aee4eb3d30b20b7635bb0ce08d3652081b7aa14e
Joshua Singleton <jsingle345@gmail.com>
2020-05-03 03:25:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
 require 'pry'
 
+
 def reverse_each_word(string)
+  new_array = []
   array = string.split(" ")
-  array.collect { |string| string.reverse } string.join(" ") 
+  array.collect { |string| string.reverse } 
+  new_array << [array.collect { |string| string.reverse}][join(" ")]
 end
